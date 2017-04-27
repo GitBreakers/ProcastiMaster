b4dc7d88c7cd90d42d857368fed1946dd3ca2455
Thomas Biesaart <thomas.biesaart@gmail.com>
2017-04-27 17:36:35 +0200

    Finalize preliminary domain model

--- procrastimaster-model/src/main/java/nl/xillio/gitbreakers/procrastimaster/server/model/User.java
+++ procrastimaster-model/src/main/java/nl/xillio/gitbreakers/procrastimaster/server/model/User.java
@@ -15,17 +15,38 @@
  */
 package nl.xillio.gitbreakers.procrastimaster.server.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.Entity;
 
 @Entity
 public class User extends BaseEntity {
-    private String fullName;
+    private String name;
+    private String email;
+    @JsonIgnore
+    private String password;
 
-    public String getFullName() {
-        return fullName;
+    public String getName() {
+        return name;
     }
 
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
