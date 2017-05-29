b7a12efb8ad473f4e92e1d939d14c4472079c41a
Pieter Dirk Soels <p.d.soels@hotmail.com>
2017-05-29 19:27:10 +0200

    Add encoding for authentication and creation of users

--- procrastimaster-server/src/main/java/nl/xillio/gitbreakers/procrastimaster/server/SecurityConfiguration.java
+++ procrastimaster-server/src/main/java/nl/xillio/gitbreakers/procrastimaster/server/SecurityConfiguration.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (C) 2017 Xillio GitBreakers (GitBreakers@xillio.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package nl.xillio.gitbreakers.procrastimaster.server;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -6,21 +21,25 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     private final UserDetailsService userDetailsService;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public SecurityConfiguration(UserDetailsService userDetailsService) {
+    public SecurityConfiguration(UserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
         this.userDetailsService = userDetailsService;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .anyRequest().authenticated()
+                .anyRequest()
+                .authenticated()
                 .and()
                 .csrf().disable()
                 .cors().disable()
@@ -29,6 +48,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService);
+        auth
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder);
     }
 }
--- procrastimaster-server/src/main/java/nl/xillio/gitbreakers/procrastimaster/server/services/UserService.java
+++ procrastimaster-server/src/main/java/nl/xillio/gitbreakers/procrastimaster/server/services/UserService.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (C) 2017 Xillio GitBreakers (GitBreakers@xillio.com)
- * <p>
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,18 +17,36 @@ package nl.xillio.gitbreakers.procrastimaster.server.services;
 
 import nl.xillio.gitbreakers.procrastimaster.server.model.entity.User;
 import nl.xillio.gitbreakers.procrastimaster.server.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.security.Principal;
 import java.util.Collections;
 
 @Service
 public class UserService extends AbstractService<User, UserRepository> implements UserDetailsService {
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserService(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @Override
     public void save(User entity, User owner) {
+        // This user is a new user, so we encrypt the password.
+        if (entity.getId() == null) {
+            if(entity.getPassword() == null) {
+                throw new IllegalArgumentException("There is no password provided.");
+            }
+
+            entity.setPassword(passwordEncoder.encode(entity.getPassword()));
+        }
         getRepository().save(entity);
     }
 
@@ -40,5 +58,30 @@ public class UserService extends AbstractService<User, UserRepository> implement
                         user.getPassword(),
                         Collections.emptyList()))
                 .orElseThrow(() -> new UsernameNotFoundException(email));
+    }
+
+    public User getUser(Principal principal) {
+        if (principal instanceof Authentication) {
+            Authentication authentication = (Authentication) principal;
+            Object userDetails = authentication.getPrincipal();
+            if (userDetails instanceof CustomUserDetails) {
+                CustomUserDetails customUserDetails = (CustomUserDetails) userDetails;
+                return customUserDetails.user;
+            }
+        }
+
+        throw new IllegalArgumentException();
+    }
+
+    private static class CustomUserDetails extends org.springframework.security.core.userdetails.User {
+
+        private final User user;
+
+        public CustomUserDetails(User user) {
+            super(user.getEmail(), user.getPassword(), Collections.emptyList());
+
+            this.user = user;
+        }
+
     }
 }
