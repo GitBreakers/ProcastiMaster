a28186ac2f502cdc76860263bfabf0ed86d11d1f
Jorn Verhoeven <jorn.verhoeven@xillio.nl>
2017-07-06 16:30:03 +0200

    Add Login view
    Added empty controller for Login view

---
+++ procrastimaster-client/src/main/java/nl/xillio/gitbreakers/procrastimaster/client/controllers/LoginController.java
@@ -0,0 +1,20 @@
+package nl.xillio.gitbreakers.procrastimaster.client.controllers;
+
+import javafx.fxml.Initializable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by Jorn on 06/07/2017.
+ */
+public class LoginController implements Initializable {
+    private static final Logger LOGGER = LoggerFactory.getLogger(LoginController.class);
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        LOGGER.debug("HI");
+    }
+}
