987641ac974cbe0c224d5aaed3350aee2e372f36
Jorn Verhoeven <jorn.verhoeven@xillio.nl>
2017-07-07 09:23:31 +0200

    Extend LoginController

--- procrastimaster-client/src/main/java/nl/xillio/gitbreakers/procrastimaster/client/controllers/LoginController.java
+++ procrastimaster-client/src/main/java/nl/xillio/gitbreakers/procrastimaster/client/controllers/LoginController.java
@@ -1,6 +1,9 @@
 package nl.xillio.gitbreakers.procrastimaster.client.controllers;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,8 +16,37 @@ import java.util.ResourceBundle;
 public class LoginController implements Initializable {
     private static final Logger LOGGER = LoggerFactory.getLogger(LoginController.class);
 
+    @FXML
+    private TextField emailText;
+
+    @FXML
+    private PasswordField passwordText;
+
+    @FXML
+    private CheckBox rememberCheck;
+
+    @FXML
+    private Button resetButton;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private Label feedback;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        LOGGER.debug("HI");
+        feedback.setText("");
+
+        loginButton.setOnMousePressed(e -> onLoginButtonPressed(e));
+        resetButton.setOnMousePressed(e -> onResetButtonPressed(e));
+    }
+
+    private void onLoginButtonPressed(MouseEvent e) {
+
+    }
+
+    private void onResetButtonPressed(MouseEvent e) {
+
     }
 }
